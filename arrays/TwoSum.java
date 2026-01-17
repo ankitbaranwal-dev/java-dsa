7984f71e1141c02583a6c38a8456ed748f955611
Ankit Baranwal <79303192+ankitbaranwal-dev@users.noreply.github.com>
2026-01-17 19:03:57 +0530

    Update TwoSum.java

--- arrays/TwoSum.java
+++ arrays/TwoSum.java
@@ -1,19 +1,37 @@
+//brute force is at last, optimized to Time: O(n) and Space: O(n)
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] indices = new int[2];
-        for(int i=0; i<nums.length-1; i++)
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++)
         {
-            for(int j=i+1; j<nums.length; j++)
+            int remains = target-nums[i];
+            if(map.containsKey(remains))
             {
-                if(nums[i]+nums[j] == target)
-                {
-                    indices[0]=i;
-                    indices[1]=j;
-                    break;
-                }
+                return new int[]{map.get(remains), i};
             }
+            map.put(nums[i], i);
         }
-
-        return indices;
+        return new int []{};
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int[] indices = new int[2];
+//         for(int i=0; i<nums.length-1; i++)
+//         {
+//             for(int j=i+1; j<nums.length; j++)
+//             {
+//                 if(nums[i]+nums[j] == target)
+//                 {
+//                     indices[0]=i;
+//                     indices[1]=j;
+//                     break;
+//                 }
+//             }
+//         }
+
+//         return indices;
+//     }
+// }
